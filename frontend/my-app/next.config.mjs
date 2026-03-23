5c0e3b471c4f4fd2abe7ced8e7ca4f1f575039c8
krish-gaur <krishgaur85@gmail.com>
2026-03-23 13:44:26 +0530

    Update Next.js config to include turboworkspace

--- frontend/my-app/next.config.mjs
+++ frontend/my-app/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  turboworkspace: {
+    root: './frontend/my-app'
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
